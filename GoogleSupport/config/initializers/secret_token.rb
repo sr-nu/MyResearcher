103590207bcae1320840dd7d12bf47909c091d20
Srikanth <srikanth.nutigattu@gmail.com>
2012-06-15 21:15:42 +0530

    New Rails project to support Google Custom Search On the Fly

---
+++ GoogleSupport/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleSupport::Application.config.secret_token = '567d9013183e62ceffa3ca2c78a2a6e0130afb7bca8657991797404dd00dfde5edb536ef8b6238111e638556aa42519ea299f0b76568ef663b47549939f9c3f9'
