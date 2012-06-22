6bdf8002b1b8c1c445dd107087238f60e8870f37
Srikanth <srikanth.nutigattu@gmail.com>
2012-06-22 20:34:25 +0530

    Collecting URL's directly into variable, isntead of polulating on page to make is super quick

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,9 +1,12 @@
 window.onload = function() { 
   chrome.bookmarks.getTree(function(bookmarks) {
     printBookmarksOne(bookmarks);
-    setTimeout(send_request,2250);
+    send_request();
   });
 };
+
+
+var links = "";
 
 function openTab(bookmarks_id){
   // chrome.tabs.create({url: "http://localhost:3000/researches/"+bookmarks_id});
@@ -20,23 +23,18 @@ function send_request(){
   // url: "http://localhost:3000/researches?callback=openTab",
   url: "http://googlesupport.heroku.com/researches?callback=openTab",
   type: "POST",
-  data: {"bookmarks" : document.getElementById('urls').innerHTML},
+  data: {"bookmarks" : links},
   dataType: "json",
-  // contentType: "application/json; charset=utf-8", //leading to [OPTIONS] request being seng
   success: function(obj){openTab(obj);},
   error: function(jqXHR, textStatus, errorThrown){document.getElementById('status').innerHTML = textStatus+errorThrown;}
   });
 
 }
 
-
-
 function printBookmarksOne(bookmarks) {
-  // document.getElementById('urls').innerHTML = "";
   bookmarks.forEach(function(bookmark) {
     if(isUrl(bookmark.url)){
-        // document.body.innerHTML += encodeURIComponent(bookmark.url) + "__";
-        document.getElementById('urls').innerHTML += bookmark.url + "__";
+        links += bookmark.url + "__";
     }
     if (bookmark.children){
       printBookmarksOne(bookmark.children);
@@ -44,27 +42,8 @@ function printBookmarksOne(bookmarks) {
   });
 }
 
-
-function printBookmarks(id) {
- chrome.bookmarks.getChildren(id, function(children) {
-    children.forEach(function(bookmark) { 
-      console.debug(bookmark.title);
-      if(isUrl(bookmark.url)){
-        document.body.innerHTML += bookmark.url + "<br/>";
-      }
-      printBookmarks(bookmark.id);
-    });
- });
-}
-
 function isUrl(url_string){
   var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
   return re.test(url_string);
 }
 
-function getBookmarks() {
- 
-  chrome.bookmarks.getChildren("0", function(){
-     document.body.innerHTML = "<b> Just wow!! </b>";
-  });
-}
