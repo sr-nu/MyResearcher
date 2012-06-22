312f87aeb796f90cca374e52d07ecf8ab8d61be4
Srikanth <srikanth.nutigattu@gmail.com>
2012-06-22 20:12:01 +0530

    cleanup

--- plugin/popup.js
+++ plugin/popup.js
@@ -12,63 +12,27 @@ function openTab(bookmarks_id){
 
 
 function send_request(){
-//   var req = new XMLHttpRequest();
-
-//   req.onreadystatechange=function()
-//   {
-//     if (req.readyState==4)
-//       {
-//         document.body.innerHTML = "response is:" + req.responseXML + " -> "+req.responseText + "->" +req.responseJSON;
-//         openTab(req.responseText);
-//       }
-//   }
-
-//   req.open(
-//       "POST",
-//       // "http://googlesupport.heroku.com/researches?bookmarks="+document.body.innerHTML,
-//       "http://localhost:3000/researches?bookmarks="+document.body.innerHTML+"&callback=?",
-//       true);
-//   req.setRequestHeader('Accept', 'application/json');
-//   req.overrideMimeType("application/json");  
-//   req.send();
-// }
-
   if (typeof jQuery  == 'undefined') {
-     document.body.innerHTML = "jquery not found....";
+     document.getElementById('status').innerHTML = "jquery not found....";
   }
-  
-  // $.post("http://localhost:3000/researches?bookmarks="+document.body.innerHTML+"&callback=?", function(data) {
-  // $.post("http://localhost:3000/researches?bookmarks="+document.body.innerHTML, function(data) {
-  //   document.body.innerHTML = "response is:"+data;
-  //   //openTab(data);
-  // });
 
-    // document.getElementById('status').innerHTML = 'making a call';
-    // var e = document.createElement('script');
-    // e.src = 'http://localhost:3000/researches?bookmarks='+document.getElementById('urls').innerHTML;
-    // document.getElementById('fb-root').appendChild(e);
-
-
-
-
-  //Using AJAX not able to retrieve still :(
-   $.ajax({
-    // url: "http://localhost:3000/researches?callback=openTab",
-    url: "http://googlesupport.heroku.com/researches?callback=openTab",
-    type: "POST",
-    data: {"bookmarks" : document.getElementById('urls').innerHTML},
-    dataType: "json",
-    // contentType: "application/json; charset=utf-8", //leading to [OPTIONS] request being seng
-    success: function(obj){openTab(obj);},
-    error: function(jqXHR, textStatus, errorThrown){document.getElementById('status').innerHTML = textStatus+errorThrown;}
-    });
+ $.ajax({
+  // url: "http://localhost:3000/researches?callback=openTab",
+  url: "http://googlesupport.heroku.com/researches?callback=openTab",
+  type: "POST",
+  data: {"bookmarks" : document.getElementById('urls').innerHTML},
+  dataType: "json",
+  // contentType: "application/json; charset=utf-8", //leading to [OPTIONS] request being seng
+  success: function(obj){openTab(obj);},
+  error: function(jqXHR, textStatus, errorThrown){document.getElementById('status').innerHTML = textStatus+errorThrown;}
+  });
 
 }
 
 
 
 function printBookmarksOne(bookmarks) {
-  document.getElementById('urls').innerHTML = "";
+  // document.getElementById('urls').innerHTML = "";
   bookmarks.forEach(function(bookmark) {
     if(isUrl(bookmark.url)){
         // document.body.innerHTML += encodeURIComponent(bookmark.url) + "__";
