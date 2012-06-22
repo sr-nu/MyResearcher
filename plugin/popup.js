4d7a2407b08b67d66b2871f6409e617834d56d3e
Srikanth <srikanth.nutigattu@gmail.com>
2012-06-23 03:25:24 +0530

    updated details for publishing

--- plugin/popup.js
+++ plugin/popup.js
@@ -1,4 +1,12 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32860962-1']);
+_gaq.push(['_trackPageview']);
+
 window.onload = function() { 
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
   chrome.bookmarks.getTree(function(bookmarks) {
     printBookmarksOne(bookmarks);
     send_request();
