64ffc1961e7f9e85b741b5159ac9761c8215a3ba
Srikanth <srikanth.nutigattu@gmail.com>
2012-06-22 23:53:21 +0530

    added support for book marks

--- GoogleSupport/app/controllers/researches_controller.rb
+++ GoogleSupport/app/controllers/researches_controller.rb
@@ -8,8 +8,12 @@ class ResearchesController < ActionController::Base
   end
 
   def show
-  	@research = Research.find(params[:id])
-  	respond_with(@research)
+    if Research.exists?(params[:id])
+      @research = Research.find(params[:id])
+      Research.find(params[:id]).delete
+    else
+        flash[:error] = "Your content is no longer available, Kindly close this window and click on your chrome extention to search...."
+    end
   end
 
   def create
